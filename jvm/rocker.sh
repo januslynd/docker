9b481178baeec9c3cec302b975be34ebe5faa894
Mario Garcia <mario.ggar@gmail.com>
2016-02-20 13:03:12 +0100

    Add base jvm docker image

---
+++ jvm/rocker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#    ___             _         _    _          _         _
+#   | _ \_  _ _ _   (_)_ _  __(_)__| |___   __| |___  __| |
+#   |   / || | ' \  | | ' \(_-< / _` / -_) / _` / _ \/ _| / / -_) '_|
+#   |_|_\\_,_|_||_| |_|_||_/__/_\__,_\___| \__,_\___/\__|_\_\___|_|
+#
+# 1) Put this in your $PATH
+# 2) Go to your JVM project folder
+# 3) Execute run-env.sh
+# 4) Profit!
+#
+
+# Developer home
+DHOME=/home/dev
+
+# Make sure you give proper permissions to share the following folders
+EXPORTED_DIRECTORIES=".m2 .gradle .lein .scala .grails .sdkman/archives .sdkman/candidates .sbt"
+
+# This function exposes previous folders to the docker container
+function exported_directories_string {
+    str=""
+
+    for d in $EXPORTED_DIRECTORIES; do
+	str+="-v $HOME/$d:$DHOME/$d "
+    done
+
+    echo $str
+}
+
+docker run -v $(pwd):/home/dev/ws $(exported_directories_string) -it com.github.janyslynd/jvm
