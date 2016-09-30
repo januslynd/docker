cce32210ddf9f0c4f9c89137ec2e4851871d9dd6
Mario Garcia <mario.ggar@gmail.com>
2016-09-30 16:13:04 +0200

    Get rid of lein config in favor of sdk and adding nvm

--- jvm/rocker.sh
+++ jvm/rocker.sh
@@ -15,7 +15,7 @@
 DHOME=/home/dev
 
 # Make sure you give proper permissions to share the following folders
-EXPORTED_DIRECTORIES=".m2 .gradle .lein .scala .groovy .grails .sdkman/archives .sdkman/candidates .sbt ides"
+EXPORTED_DIRECTORIES=".m2 .gradle .lein .scala .groovy .grails .sdkman/archives .sdkman/candidates .sbt .nvm ides"
 
 # This function exposes previous folders to the docker container
 function exported_directories_string {
