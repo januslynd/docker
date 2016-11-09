cd9441713f11ef2188f17164760a68aa55f3756c
Mario Garcia <mario.ggar@gmail.com>
2016-11-09 08:02:47 +0100

    Add refactor to standarize Docker builds

--- jvm/bin/rocker.sh
+++ jvm/bin/rocker.sh
@@ -15,11 +15,15 @@
 DHOME=/home/dev
 
 # Make sure you give proper permissions to share the following folders
-EXPORTED_DIRECTORIES=".m2 .gradle .lein .scala .groovy .grails .sdkman/archives .sdkman/candidates .sbt .nvm ides"
+EXPORTED_DIRECTORIES=".m2 .gradle .lein .scala .groovy .grails .sdkman/archives .sdkman/candidates .sbt .nvm .intellij"
 
 # This function exposes previous folders to the docker container
 function exported_directories_string {
     str=""
+
+    for d in $EXPORTED_DIRECTORIES; do
+        mkdir -p "$HOME/$d"
+    done
 
     for d in $EXPORTED_DIRECTORIES; do
 	str+="-v $HOME/$d:$DHOME/$d "
