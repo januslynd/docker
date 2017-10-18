b8063ce8686651c409150b8c3b726b7d274c6eb8
Mario Garcia <mario.ggar@gmail.com>
2017-10-18 08:57:33 +0200

    fix setting up groovyserv once variables have been set

--- groovyserv/files/entrypoint.sh
+++ groovyserv/files/entrypoint.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
 set -e
-set -x
 
 echo "Setting GroovyServ variables..."
 GROOVYSERV_PORT=${GROOVYSERV_PORT:-"9999"}
 GROOVYSERV_LOG_DIR=${GROOVYSERV_LOG_DIR:-"/var/log/"}
 GROOVYSERV_WORK_DIR=${GROOVYSERV_WORK_DIR:-"/opt/groovyserv/auth/"}
-GROOVYSERV_AUTHTOKEN=${GROOVYSERV_AUTHTOKEN:-"secrettoken"}
 
 export GROOVYSERV_LOG_DIR
 export GROOVYSERV_WORK_DIR
 
+echo "Setting up GroovyServ..."
+/opt/groovyserv/groovyserv-$GROOVYSERV_VERSION/bin/setup.sh
+
 echo "Starting GroovyServ..."
-groovyserver --debug -p $GROOVYSERV_PORT --authtoken $GROOVYSERV_AUTHTOKEN -v -t 1000
+groovyserver --debug -p $GROOVYSERV_PORT -v -t 1000
 
 echo "Starting fake Xorg server"
 Xvfb :10 -ac
