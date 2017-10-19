7d640d6aa61ed0b559163e4e68bae4a6b8ae4778
Mario Garcia <mario.ggar@gmail.com>
2017-10-19 09:50:41 +0200

    Get rid of selenium/firefox/gecko

--- groovyserv/README.md
+++ groovyserv/README.md
@@ -11,11 +11,3 @@ once you are in the `rpi-java` directory you have to:
 Groovyserv (https://github.com/kobo/groovyserv) makes Groovy execution
 faster because it starts up a new JVM and executes new Groovy
 executions in that JVM.
-
-## Extra: Geb->Firefox friendly
-
-This image can also be used to access Firefox's `geckodriver` via
-`Geb` (gebish.org). Things to keep in mind:
-
-- Geckodriver needs the firewall to allow connections comming from
-  same ip (Need to know how to narrow this)
--- groovyserv/files/entrypoint.sh
+++ groovyserv/files/entrypoint.sh
@@ -4,17 +4,18 @@ set -e
 
 echo "Setting GroovyServ variables..."
 GROOVYSERV_PORT=${GROOVYSERV_PORT:-"9999"}
-GROOVYSERV_LOG_DIR=${GROOVYSERV_LOG_DIR:-"/var/log/"}
-GROOVYSERV_WORK_DIR=${GROOVYSERV_WORK_DIR:-"/opt/groovyserv/auth/"}
+GROOVYSERV_LOG_DIR=${GROOVYSERV_LOG_DIR:-"$GROOVYSERV_PATH/log/"}
+GROOVYSERV_WORK_DIR=${GROOVYSERV_WORK_DIR:-"$GROOVYSERV_PATH/work/"}
+GROOVYSERV_AUTHTOKEN=${GROOVYSERV_AUTHTOKEN:-"secrettoken"}
 
 export GROOVYSERV_LOG_DIR
 export GROOVYSERV_WORK_DIR
 
 echo "Setting up GroovyServ..."
-/opt/groovyserv/groovyserv-$GROOVYSERV_VERSION/bin/setup.sh
+$GROOVYSERV_PATH/groovyserv-$GROOVYSERV_VERSION/bin/setup.sh
 
 echo "Starting GroovyServ..."
-groovyserver --debug -p $GROOVYSERV_PORT -v -t 1000
+groovyserver --debug -p $GROOVYSERV_PORT --authtoken $GROOVYSERV_AUTHTOKEN -v -t 1000
 
-echo "Starting fake Xorg server"
-Xvfb :10 -ac
+echo "Keeping container alive"
+tail -f $GROOVY_HOME/LICENSE
